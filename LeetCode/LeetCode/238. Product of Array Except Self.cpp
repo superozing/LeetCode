f0e199c398d50e807a12f0f14e5bb6d731f34f59
superozing <amazingozing@gmail.com>
2023-12-14 22:49:31 +0900

    238. Product of Array Except Self 설명 봐도 이해 못했습니당.

--- LeetCode/LeetCode/238. Product of Array Except Self.cpp
+++ LeetCode/LeetCode/238. Product of Array Except Self.cpp
@@ -1,27 +1,15 @@
 #include "헤더.h"
 
-class Solution 
+class Solution
 {
 public:
-    vector<int> productExceptSelf(vector<int>& nums) 
+    vector<int> productExceptSelf(vector<int>& nums)
     {
-        // 나눗셈 금지
-        // 그러면 1/n으로 곱하면 되는거 아닌가?
-        // /이 들어가서 안되는건가?
-        // 규칙을 찾지 못하겠어요.
-        vector<int> vReturn(nums.size(), 1);
+        // 나눗셈 연산을 사용하지 않고 n에 끝내기.
 
-        for (int i = 0; i < nums.size(); ++i)
-        {
-            int iBuf = nums[i];
-
-            for (int j = 0; j < nums.size(); ++j)
-            {
-                if (i == j) continue;
-                vReturn[j] *= iBuf;
-            }
-
-        }
+        vector<int> Left;
+        vector<int> Right;
+        vector<int> vReturn;
 
         return vReturn;
     }
