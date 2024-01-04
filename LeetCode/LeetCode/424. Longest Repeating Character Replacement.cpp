36cdf84661a80cf720588b65d3af9a9ec40fdcb8
superozing <amazingozing@gmail.com>
2024-01-04 20:12:55 +0900

    424. Longest Repeating Character Replacement
    도저히 감이 잡히지 않아서 GPT와 해설의 도움을 받아 풀었어요.

--- LeetCode/LeetCode/424. Longest Repeating Character Replacement.cpp
+++ LeetCode/LeetCode/424. Longest Repeating Character Replacement.cpp
@@ -1,7 +1,4 @@
 #include "헤더.h"
-
-
-
 class Solution
 {
 public:
@@ -16,39 +13,37 @@ public:
         // 일단 슬라이딩 윈도우는 앞부분 늘리고 뒷부분 줄이고. 를 하라는 뜻이잖아?
         // 계속해서 데이터를 입력받고, 빈도가 가장 높은 문자의 주변을 k개만큼 바꾸었을 때의 길이를 저장
 
-        vector<int> 빈도수('Z' + 1);
+        // 빈도 체크
+        vector<int> FrequencyCount('Z' + 1); // 앞 쪽 공간이 아깝기는 하네요...?
+        
+        // 슬라이딩 윈도우
         int LIdx = 0;
         int RIdx = 0;
-        int 최대빈도 = 0;
 
-        int Return = 0;
+        // 최대 빈도
+        int MaxFrequency = 0;
+
+        int ReturnVal = 0;
 
         while (RIdx != s.length()) // 오른 쪽 포인터가 끝에 다다를 때 까지 반복
         {
             // 최대 윈도우 거리 계산
-            int MaxWndLen = RIdx - LIdx + 1;
+            int CurWndLen = RIdx - LIdx + 1;
 
-            // 새로 들어온 데이터를 증가시키고 최대빈도 갱신
-            ++빈도수[s[RIdx]];
-            최대빈도 = max(빈도수[s[RIdx]], 최대빈도);
+            // 새로 들어온 데이터를 증가시키고 MaxFrequency 갱신
+            ++FrequencyCount[s[RIdx]];
+            MaxFrequency = max(FrequencyCount[s[RIdx]], MaxFrequency);
 
-            // 윈도우 개수가 k + 최대 빈도보다 크면 문제가 있음.
-            if (MaxWndLen > k + 최대빈도)
-                --빈도수[s[LIdx++]];
-            else // 아니면 정상적이래요.
-                Return = max(Return, MaxWndLen);
+            // 윈도우 개수가 k + MaxFrequency 보다 크면 문제가 있음. (일치하지 않는 k개의 문자를 바꾸어도 아직 바꾸지 않은 문자가 있다는 것)
+            if (CurWndLen > k + MaxFrequency)
+                --FrequencyCount[s[LIdx++]];
+            // 윈도우의 문자를 k개 만큼 바꾸었을 때 전부 동일한 경우
+            else 
+                ReturnVal = max(ReturnVal, CurWndLen); // ReturnVal에 저장
 
             ++RIdx;
         }
 
-        return Return;
+        return ReturnVal;
     }
 };
-
-
-int main()
-{
-    Solution s;
-    printf("%d\n", s.characterReplacement("ABAB", 2));
-    return 0;
-}
